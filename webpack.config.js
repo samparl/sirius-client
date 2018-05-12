cbc70378f4f4d56de367c93f065aa8ca296bfeef
Sam Parl <samaparl@gmail.com>
2018-05-12 16:35:53 -0400

    Use tsx format in app component

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,19 @@ module.exports = {
     contentBase: __dirname,
     port: 3001
   },
+  devTool: 'source-map',
   module: {
     rules: [
+      // {
+      //   test: /\.(js|jsx)$/,
+      //   exclude: /(node_modules)/,
+      //   loader: 'babel-loader',
+      //   options: { presets: ['env'] }
+      // },
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx|ts|tsx)$/,
         exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        options: { presets: ['env'] }
+        loader: 'awesome-typescript-loader'
       },
       {
         test: /\.css$/,
@@ -26,7 +32,7 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
     alias: {
       services: path.resolve(__dirname, 'src/services'),
       utils: path.resolve(__dirname, 'src/utils')
