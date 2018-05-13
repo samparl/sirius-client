62fa459524d8af274bea5becd736af5d0a4bfa24
Sam Parl <samaparl@gmail.com>
2018-05-13 14:32:53 -0400

    Add text introduction on main app component

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,12 @@ class App extends React.Component<null, null> {
           </span>
         </header>
         <div className="App-body">
+          <div className="DefaultView">
+            <div className="DefaultView-introduction">
+              Welcome to Sirius! We do our best to connect people with the products they love and need.
+              Watch your new purchase make its way to your doorstep, from start to finish!
+            </div>
+          </div>
           <Dashboard />
         </div>
       </div>
