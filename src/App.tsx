3555e26592348aaaabd83a650d61296f50c8eab7
Sam Parl <samaparl@gmail.com>
2018-05-13 18:55:23 -0400

    Style header icon

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ class App extends React.Component<null, null> {
           </span>
         </header>
         <div className="App-body">
+          <DefaultView />
           <Dashboard />
         </div>
       </div>
