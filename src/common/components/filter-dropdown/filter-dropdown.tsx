1d7a7c9cc90c89d1d6a81ff0585edbb6b85f2e20
Sam Parl <samaparl@gmail.com>
2018-05-15 18:36:11 -0400

    Fix unmount bug in filter dropdown

--- src/common/components/filter-dropdown/filter-dropdown.tsx
+++ src/common/components/filter-dropdown/filter-dropdown.tsx
@@ -2,82 +2,82 @@ import * as React from 'react';
 import './filter-dropdown.css';
 
 interface FilterDropdownOption {
-    name: string;
-    value?: any;
+  name: string;
+  value?: any;
 }
 
 class FilterDropdownState {
-    selected: number;
-    open: boolean;
+  selected: number;
+  open: boolean;
 
-    constructor() {
-        this.selected = null;
-        this.open = false;
-    }
+  constructor() {
+    this.selected = null;
+    this.open = false;
+  }
 }
 
 interface FilterDropdownProps {
-    options: FilterDropdownOption[];
-    onSelect?: (option: FilterDropdownOption, index: number, event: Event) => {};
+  options: FilterDropdownOption[];
+  onSelect?: (option: FilterDropdownOption, index: number, event: Event) => {};
 }
 
 export class FilterDropdown extends React.Component<FilterDropdownProps, FilterDropdownState> {
-    node: HTMLDivElement;
+  node: HTMLDivElement;
 
-    constructor(props: any) {
-        super(props);
-        this.state = new FilterDropdownState();
+  constructor(props: any) {
+    super(props);
+    this.state = new FilterDropdownState();
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.close.bind(this));
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.close.bind(this));
+  }
+
+  private isSelected(index: number) {
+    return index === this.state.selected;
+  }
+
+  toggle() {
+    this.setState({ open: !this.state.open });
+  }
+
+  close(event: Event) {
+    if (this.node && !this.node.contains(event.target as Node) && this.state.open) {
+      this.toggle()
     }
+  }
 
-    componentDidMount() {
-        document.addEventListener('click', this.close.bind(this));
-    }
+  select(option: any, index: number, e: Event) {
+    e.stopPropagation();
+    if (this.props.onSelect) this.props.onSelect(option, index, e);
+    this.setState({ selected: index });
+  }
 
-    componentWillUnmount() {
-        document.removeEventListener('click', this.close.bind(this));
-    }
-
-    private isSelected(index: number) {
-        return index === this.state.selected;
-    }
-
-    toggle() {
-        this.setState({ open: !this.state.open });
-    }
-
-    close(event: Event) {
-        if (!this.node.contains(event.target as Node) && this.state.open) {
-            this.toggle()
+  render() {
+    const options = !this.state.open ? null :
+      <div className="filter-dropdown-options">
+        {
+          this.props.options.map((option, index) => {
+            const className = 'filter-dropdown-option' + (this.isSelected(index) ? ' selected' : '');
+            return <div className={className}
+              key={index}
+              onClick={this.select.bind(this, option, index)}>
+              {option.name}
+            </div>
+          })
         }
-    }
+      </div>
 
-    select(option: any, index: number, e: Event) {
-        e.stopPropagation();
-        if (this.props.onSelect) this.props.onSelect(option, index, e);
-        this.setState({selected: index});
-    }
-
-    render() {
-        const options = !this.state.open ? null :
-            <div className="filter-dropdown-options">
-                {
-                    this.props.options.map((option, index) => {
-                        const className = 'filter-dropdown-option' + (this.isSelected(index) ? ' selected' : '');
-                        return <div className={className}
-                            key={index}
-                            onClick={this.select.bind(this, option, index)}>
-                            {option.name}
-                        </div>
-                    })
-                }
-            </div>
-
-        const selected = this.props.options[this.state.selected] && this.props.options[this.state.selected].name;
-        return (
-            <div className="FilterDropdown" onClick={ this.toggle.bind(this) } ref={ node => this.node = node }>
-                <div className="selected-option">{ selected || 'Select' }</div>
-                { options }
-            </div>
-        );
-    }
+    const selected = this.props.options[this.state.selected] && this.props.options[this.state.selected].name;
+    return (
+      <div className="FilterDropdown" onClick={this.toggle.bind(this)} ref={node => this.node = node}>
+        <div className="selected-option">{selected || 'Select'}</div>
+        {options}
+      </div>
+    );
+  }
 }
